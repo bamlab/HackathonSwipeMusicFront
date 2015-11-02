7b0db3011b522a3debbc7f461dd91873dbd46f66
EliotAndres <eliota@bamlab.fr>
2015-11-02 18:54:10 +0100

    First test with android

--- index.android.js
+++ index.android.js
@@ -1,41 +1,145 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
-var React = require('react-native');
-var AudioPlayer = require('react-native-audioplayer');
+import React, { AppRegistry, Image, StyleSheet, Text, View, Animated, TouchableNativeFeedback, Component, PanResponder, } from 'react-native';
+import clamp from 'clamp';
 
-var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} = React;
+const People = [
+  'red',
+  'green',
+  'blue',
+  'purple',
+  'orange',
+]
 
+var SWIPE_THRESHOLD = 120;
 
-var HackathonSwipeMusicFront = React.createClass({
-  playMusic: function() {
-    AudioPlayer.play('moo.mp3');
-  },
+class HackathonSwipeMusicFront extends Component {
 
-  render: function() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pan: new Animated.ValueXY(),
+      enter: new Animated.Value(0.5),
+      person: People[0],
+    }
+  }
+
+  _goToNextPerson() {
+    let currentPersonIdx = People.indexOf(this.state.person);
+    let newIdx = currentPersonIdx + 1;
+
+    this.setState({
+      person: People[newIdx > People.length - 1 ? 0 : newIdx]
+    });
+  }
+
+  componentDidMount() {
+    this._animateEntrance();
+  }
+
+  _animateEntrance() {
+    Animated.spring(
+      this.state.enter,
+      { toValue: 1, friction: 8 }
+    ).start();
+  }
+
+  componentWillMount() {
+    this._panResponder = PanResponder.create({
+      onMoveShouldSetResponderCapture: () => true,
+      onMoveShouldSetPanResponderCapture: () => true,
+
+      onPanResponderGrant: (e, gestureState) => {
+        this.state.pan.setOffset({x: this.state.pan.x._value, y: this.state.pan.y._value});
+        this.state.pan.setValue({x: 0, y: 0});
+      },
+
+      onPanResponderMove: Animated.event([
+        null, {dx: this.state.pan.x, dy: this.state.pan.y},
+      ]),
+
+      onPanResponderRelease: (e, {vx, vy}) => {
+        this.state.pan.flattenOffset();
+        var velocity;
+
+        if (vx >= 0) {
+          velocity = clamp(vx, 3, 5);
+        } else if (vx < 0) {
+          velocity = clamp(vx * -1, 3, 5) * -1;
+        }
+
+        if (Math.abs(this.state.pan.x._value) > SWIPE_THRESHOLD) {
+          Animated.decay(this.state.pan, {
+            velocity: {x: velocity, y: vy},
+            deceleration: 0.98
+          }).start(this._resetState.bind(this))
+        } else {
+          Animated.spring(this.state.pan, {
+            toValue: {x: 0, y: 0},
+            friction: 4
+          }).start()
+        }
+      }
+    })
+  }
+
+  _resetState() {
+    this.state.pan.setValue({x: 0, y: 0});
+    this.state.enter.setValue(0);
+    this._goToNextPerson();
+    this._animateEntrance();
+  }
+
+  render() {
+    let { pan, enter, } = this.state;
+
+    let [translateX, translateY] = [pan.x, pan.y];
+
+    let rotate = pan.x.interpolate({inputRange: [-200, 0, 200], outputRange: ["-30deg", "0deg", "30deg"]});
+    let opacity = pan.x.interpolate({inputRange: [-200, 0, 200], outputRange: [0.5, 1, 0.5]})
+    let scale = enter;
+
+    let animatedCardStyles = {transform: [{translateX}, {translateY}, {rotate}, {scale}], opacity};
+
+    let yupOpacity = pan.x.interpolate({inputRange: [0, 150], outputRange: [0, 1]});
+    let yupScale = pan.x.interpolate({inputRange: [0, 150], outputRange: [0.5, 1], extrapolate: 'clamp'});
+    let animatedYupStyles = {transform: [{scale: yupScale}], opacity: yupOpacity}
+
+    let nopeOpacity = pan.x.interpolate({inputRange: [-150, 0], outputRange: [1, 0]});
+    let nopeScale = pan.x.interpolate({inputRange: [-150, 0], outputRange: [1, 0.5], extrapolate: 'clamp'});
+    let animatedNopeStyles = {transform: [{scale: nopeScale}], opacity: nopeOpacity}
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Music Tinder!
-        </Text>
-        <Text style={styles.mooText} onPress={this.playMusic}>
-          Moo
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+        <Animated.View                         // Base: Image, Text, View
+         style={[styles.card, animatedCardStyles, {backgroundColor: this.state.person}]} {...this._panResponder.panHandlers}>
+         <Image
+          style={styles.logo}
+          source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+        />
+
+
+        <TouchableNativeFeedback
+        onPress={this._onPressButton}
+        background={TouchableNativeFeedback.SelectableBackground()}>
+      <View style={{width: 150, height: 100, backgroundColor: 'red'}}>
+        <Text style={{margin: 30}}>Button</Text>
+      </View>
+    </TouchableNativeFeedback>
+        </Animated.View>
+
+        <Animated.View style={[styles.nope, animatedNopeStyles]}>
+          <Text style={styles.nopeText}>Nope!</Text>
+        </Animated.View>
+
+        <Animated.View style={[styles.yup, animatedYupStyles]}>
+          <Text style={styles.yupText}>Yup!</Text>
+        </Animated.View>
       </View>
     );
   }
-});
+}
 
 var styles = StyleSheet.create({
   container: {
@@ -44,22 +148,41 @@ var styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  card: {
+    width: 200,
+    height: 300,
+    backgroundColor: 'red',
   },
-  mooText: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    color: '#990000',
+  yup: {
+    borderColor: 'green',
+    borderWidth: 2,
+    position: 'absolute',
+    padding: 20,
+    bottom: 20,
+    borderRadius: 5,
+    right: 20,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  yupText: {
+    fontSize: 16,
+    color: 'green',
   },
+  nope: {
+    borderColor: 'red',
+    borderWidth: 2,
+    position: 'absolute',
+    bottom: 20,
+    padding: 20,
+    borderRadius: 5,
+    left: 20,
+  },
+  nopeText: {
+    fontSize: 16,
+    color: 'red',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+  }
 });
 
 AppRegistry.registerComponent('HackathonSwipeMusicFront', () => HackathonSwipeMusicFront);
